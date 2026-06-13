45506b10ec0ff3fb14505ecaa965b6e87f598aaf
rahmonovssss <135398651+rahmonovssss@users.noreply.github.com>
2026-06-13 18:32:42 +0500

    Update MainActivity.java

--- app/src/main/java/com/example/myquizapp/MainActivity.java
+++ app/src/main/java/com/example/myquizapp/MainActivity.java
@@ -1,23 +1,16 @@
 package com.example.myquizapp;
 
 import android.os.Bundle;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // WebView-ni yaratish
-        WebView webView = new WebView(this);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
-        
-        // Assets ichidagi HTML-ni yuklash
-        webView.loadUrl("file:///android_asset/index.html");
-        
-        setContentView(webView);
+        // Oddiy "Hello World" ekran
+        setContentView(new android.widget.TextView(this) {{
+            setText("Ilova ishlamoqda!");
+            setTextSize(30);
+        }});
     }
 }
